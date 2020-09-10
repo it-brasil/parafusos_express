67be025040d3e404750090aa41e0c7edc8ad4bb7
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-09-10 13:39:53 -0300

    Novos modulos de setar a quantidade e criação de pre-pedidos

---
+++ pos_set_quantity/static/src/js/pos_set_quantity.js
@@ -0,0 +1,51 @@
+odoo.define('pos_set_quantity.screens', function (require) {
+    "use strict";
+    
+    var pos_screens = require('point_of_sale.screens');
+    var gui = require('point_of_sale.gui');
+    var PopupWidget = require('point_of_sale.popups');
+
+    pos_screens.ProductScreenWidget.include({
+        click_product: function(product) {
+            if(product.to_weight && this.pos.config.iface_electronic_scale){
+                this.gui.show_screen('scale', {product: product});
+            }else{
+                this.gui.show_popup('pos-set-quantity', {product: product});
+            }
+         },
+    });
+
+    var SetQtyPopupWidget = PopupWidget.extend({
+        template: 'SetQtyPopupWidget',
+        init(parent, options) {
+            this._super(parent, options);
+            this.options = options;
+        },
+        show: function (options) {
+            var self = this;
+            this._super(options);
+
+            this.$(".confirm").click(function(ev) {
+                var quantity = parseInt(self.$(".input-set-qty").val());
+                if(quantity > 0){
+                    self.pos.get_order().add_product(self.options.product, { quantity: quantity });
+                } else {
+                    alert("A quantidade deve ser maior que 0!");
+                    ev.stopPropagation();
+                }
+            });
+
+            this.$(".input-set-qty").keypress(function(ev) {
+                var keyCode = ev.keyCode || ev.which;
+                if(keyCode == '13'){
+                    self.$(".confirm").trigger("click");
+                }
+            })
+
+            this.$(".input-set-qty").focus();
+        }
+    });
+    gui.define_popup({name:'pos-set-quantity', widget: SetQtyPopupWidget});
+
+
+});
