6b6943cbad26dbad545dc220ce49c5cf122fa76a
Danimar Ribeiro <danimaribeiro@gmail.com>
2020-11-03 08:50:06 -0300

    Customizacoes do pos

---
+++ pos_customization/__manifest__.py
@@ -0,0 +1,17 @@
+{  # pylint: disable=C8101,C8103
+    "name": "Pos Set Quantity",
+    "description": "Open box to set quantity when \
+        adding one product to the POS sale.",
+    "category": "Product",
+    "version": "12.0.1.0.0",
+    "contributors": [
+        "Danimar Ribeiro <danimaribeiro@gmail.com>"
+    ],
+    "depends": [
+        "point_of_sale",
+        "br_account",
+    ],
+    "data": [
+        "views/res_partner.xml"
+    ],
+}
